2281b59518bf9b40579bfb7af20e2cf1f54c8018
Ryan Alamo <ralamo33@gmail.com>
2025-01-16 11:35:19 -0500

    a

---
+++ src/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+from flask_restx import Resource, Api
+
+app = Flask(__name__)
+
+api = Api(app)
+
+app.config.from_object("src.config.DevelopmentConfig")
+
+class Ping(Resource):
+    def get(self):
+        return {
+            "status": "success",
+            "message": "different!"
+        }
+
+api.add_resource(Ping, '/ping')
